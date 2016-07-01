bc8cdca7b5755cfa22e771b2201df1880a9a2d3f
craft <yooobuntu@163.com>
2016-07-01 18:26:25 +0800

    tiny refact

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,27 @@
 /* jshint devel:true */
 console.log('Look at app/js/main.js');
-
-function initEvents() {
-  document.querySelector(".t-player-processbar").style.width = 0;
-  let playerButton = document.querySelector('#t-player-button');
-  playerButton.addEventListener('click', () => {
-    console.log('click');
+(function(context, events){
+  document.addEventListener('DOMContentLoaded', events['init'], false);
+  context.addEventListener('click', (e) => {
+    if (e.target && e.target.matches('#t-player-button')) {
+      events['playerEvent'].call(context.querySelector('.t-player-processbar'));
+    }
+    else if (e.target && e.target.matches('#t-stop-player-button')) {
+      events['stopEvent'].call(null);
+    }
+  }, false)
+}).call(this, document.querySelector('body'), {
+  init: function () {
+    document.querySelector(".t-player-processbar").style.width = 0;
+  },
+  playerEvent: function () {
+    console.log(this);
     window.playerId = setInterval(() => {
-      var processbar = document.querySelector(".t-player-processbar");
-      processbar.style.width = [new String(parseInt(processbar.style.width) + 1), ''].join("%");
-      console.log('width', processbar.style.width);
+      this.style.width = `${parseInt(this.style.width) + 1}%`;
+      console.log('width', this.style.width);
     }, 3000);
-  }, false);
-
-  let stopPlayerButton = document.querySelector('#t-stop-player-button');
-  stopPlayerButton.addEventListener('click', () => {
-    console.log('stop');
+  },
+  stopEvent: function() {
     clearInterval(window.playerId);
-  }, false);
-
-}
-
-const body = document.querySelector("body");
-
-body.addEventListener("load", initEvents(), false);
+  }
+});
